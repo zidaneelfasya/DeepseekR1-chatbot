96e65f5c86b10cedfc22964d689a79b9780fa257
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-02-21 15:55:22 +0700

    Update ChatSidebar.tsx

--- src/components/ChatSidebar.tsx
+++ src/components/ChatSidebar.tsx
@@ -47,7 +47,6 @@ export const ChatSidebar = () => {
 
   const handleCreateThread = async () => {
     const threadId = await db.createThread(textInput);
-
     setIsDialogOpen(false);
     setTextInput("");
   };
